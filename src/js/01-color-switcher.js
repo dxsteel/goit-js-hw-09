4d8606488a0b9afb014002f189067e20d92753df
Dmytro Khomenko <dmytro.khomenko.it@gmail.com>
2022-05-04 00:12:11 +0300

    color-swither

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('button[data-start]'),
+    stopButton: document.querySelector('button[data-stop]'),
+};
+
+let timer;
+
+refs.stopButton.addEventListener('click', stopClick);
+refs.startButton.addEventListener('click', startClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
+}
+
+function startClick() {
+    console.log('start');
+    timer = setInterval(() => {
+        let color = getRandomHexColor();
+        refs.body.style.backgroundColor = color;
+    }, 1000);
+    refs.startButton.disabled = 'disabled';
+    refs.stopButton.disabled = '';
+}
+
+function stopClick() {
+    console.log('stop');
+    clearInterval(timer);
+    refs.startButton.disabled = '';
+    refs.stopButton.disabled = 'disabled';
+}
 
